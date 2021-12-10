c84569e282cbe045f0d624523f5cf0f8e95951b8
Marcela <marcelapereirads@gmail.com>
2021-12-10 17:51:35 +0000

    Refactor and simplify examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,113 @@
-import { Component, OnInit } from '@angular/core';
-import { AppService } from './app.service';
+import { Component } from '@angular/core';
+import { concat, forkJoin, from, interval, of } from 'rxjs';
+import { concatMap, exhaustMap, map, mergeMap, switchMap, take, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-  
-  constructor(private service: AppService) { }
+export class AppComponent {
 
-  ngOnInit() {
-    this.service.ofExample().subscribe(values => console.log('Of:', values));
-    this.service.fromExample().subscribe(values => console.log('From:', values));
-    this.service.forkJoinExample().subscribe(values => console.log('Fork Join:', values));
+  parentObservable$ = interval(1000).pipe(map(i => `Parent ${i}`));
+  childObservable$ = (parent) => interval(2000).pipe(map(i => `Child of ${parent} => ${(i) * 10}`));
+
+  ofExample() {
+    console.log('====================== OF ======================');
+
+    // The 'of' function emits all elements once
+    of(['blue', 'green', 'red']).subscribe(item => console.log(item));
+  }
+
+  fromExample() {
+    console.log('====================== FROM ======================');
+
+    // The 'from' function emits the elements one by one
+    from(['blue', 'green', 'red']).subscribe(item => console.log(item));
+  }
+
+  mapExample() {
+    console.log('====================== MAP ======================');
+
+    const numbers = from([1, 2, 3]).pipe(
+      // The 'map' transforms the emitted value
+      map(number => 'The number is ' + number)
+    );
+    numbers.subscribe(value => console.log(value));
+  }
+
+  concatExample() {
+    console.log('====================== CONCAT ======================');
+
+    // The 'concat' creates an observable that emits values from all sources
+    const concatedValues = concat(of(1), of(2));
+
+    concatedValues.subscribe(value => console.log(value));
+  }
+
+  forkJoinExample() {
+    console.log('====================== FORK JOIN ======================');
+    
+    // The 'forkJoin' emits an array or object with all the passed values
+    forkJoin({
+      firstObservable: of(1),
+      secondObsevable: of(2)
+    }).subscribe(value => console.log(value));
+  }
+
+  concatMapExample() {
+    console.log('====================== CONCAT MAP ======================');
+
+    this.parentObservable$.pipe(
+      tap(console.log),
+      take(5),
+      // The 'concatMap' waits for the end of the previous observable to complete before creaating the second
+      concatMap((value) => this.childObservable$(value).pipe(
+        tap(console.log),
+        take(3)
+      ))
+    ).subscribe();
+  }
+
+  mergeMapExample() {
+    console.log('====================== MERGE MAP ======================');
+
+    this.parentObservable$.pipe(
+      tap(console.log),
+      take(5),
+      // The 'mergeMap' combines the values of two observables (does not guarantee that they are executed in sequence)
+      mergeMap((value) => this.childObservable$(value).pipe(
+        tap(console.log),
+        take(3)
+      ))
+    ).subscribe();
+  }
+
+  switchMapExample() {
+    console.log('====================== SWITCH MAP ======================');
+
+    this.parentObservable$.pipe(
+      tap(console.log),
+      take(5),
+      // The 'switchMap' breaks the current observable and creates the next one
+      switchMap((value) => this.childObservable$(value).pipe(
+        tap(console.log),
+        take(3)
+      ))
+    ).subscribe();
+  }
+
+  exhaustMapExample() {
+    console.log('====================== EXHAUST MAP ======================');
+    
+    this.parentObservable$.pipe(
+      tap(console.log),
+      take(5),
+      // The 'exhaustMap' ignores other observables while the previous one is not complete
+      exhaustMap((value) => this.childObservable$(value).pipe(
+        tap(console.log),
+        take(3)
+      ))
+    ).subscribe();
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,6 @@
 
 export const environment = {
   production: false,
-  pokemonAPI: 'https://pokeapi.co/api/v2'
 };
 
 /*
